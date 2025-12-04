f6d6d69edafea5aa3293ffbbb339f6d89c495a58
Matias Ramirez <matias.ramirez.e@usach.cl>
2025-12-04 00:25:17 -0300

    Fix: Match env var names with Dockerfile

--- src/services/keycloak.js
+++ src/services/keycloak.js
@@ -2,8 +2,8 @@ import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
     url: import.meta.env.VITE_KEYCLOAK_URL,
-    realm: import.meta.env.VITE_KEYCLOAK_REALM,
-    clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
+    realm: import.meta.env.VITE_REALM,
+    clientId: import.meta.env.VITE_CLIENT_ID,
 });
 
 export default keycloak;
